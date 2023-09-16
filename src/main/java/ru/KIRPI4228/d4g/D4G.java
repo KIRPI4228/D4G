1d7023df8597239cee07faf08771b1b8293c987c
kirpi4 <kirpi4@daylifecraft.com>
2023-09-16 14:19:50 +0300

    Remove test

--- src/main/java/ru/KIRPI4228/d4g/D4G.java
+++ src/main/java/ru/KIRPI4228/d4g/D4G.java
@@ -2,12 +2,6 @@ package ru.KIRPI4228.d4g;
 
 public class D4G {
     public static void main(final String[] args) {
-        String chars = "123";
-        char[] symbols = new char[chars.length()];
-        chars.getChars(0, chars.length(), symbols, 0);
-
-        Generator generator = new Generator(3, symbols);
-
-        generator.generate("D:\\huy228");
+        
     }
 }
